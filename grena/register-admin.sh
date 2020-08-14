122302a7ea817eb32cd0481c489a4ed9b12ed753
Giorgi Lekveishvili <lekva@gl.local>
2020-08-14 16:31:42 +0400

    manage: register user/admin wait for pod to finish and retrieve logs

--- grena/register-admin.sh
+++ grena/register-admin.sh
@@ -5,4 +5,15 @@ set -e
 export ADMIN_USER=$1
 export ADMIN_PASSWORD=$2
 
+echo "Cleaning up old run"
+./kubectl -n cms delete pod/register-admin
+
+echo "Starting pod"
 envsubst < ../k8s/register-admin.yaml | kubectl apply -f -
+while [[ $(kubectl -n cms get pods register-admin -o 'jsonpath={..status.phase}') != "Succeeded" ]]; do
+    echo "Waiting for pod"
+    sleep 1
+done
+
+echo "Logs"
+./kubectl -n cms logs register-admin
--- grena/register-users.sh
+++ grena/register-users.sh
@@ -4,6 +4,19 @@ set -e
 
 export USERS_FILE=$1
 
-./kubectl -n cms create configmap contestants --from-file=$USERS_FILE
-envsubst < ../k8s/register-users.yaml | ./kubectl apply -f -
+echo "Cleaning up old run"
+./kubectl -n cms delete pod/register-users
+./kubectl -n cms delete configmap/contestants
 
+echo "Creating users config file"
+./kubectl -n cms create configmap contestants --from-file=$USERS_FILE
+
+echo "Starting pod"
+envsubst < ../k8s/register-users.yaml | ./kubectl apply -f -
+while [[ $(kubectl -n cms get pods register-users -o 'jsonpath={..status.phase}') != "Succeeded" ]]; do
+    echo "Waiting for pod"
+    sleep 1
+done
+
+echo "Logs"
+./kubectl -n cms logs register-users
